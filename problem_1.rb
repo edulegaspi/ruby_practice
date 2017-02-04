440bc3e354d5219ca9c6f0731ee7d88f9e384bbe
edulegaspi <edu.torres.legaspi@gmail.com>
2017-02-04 18:29:23 +0800

    Solve problem 1

--- problem_1.rb
+++ problem_1.rb
@@ -1,1 +1,12 @@
 # Project Euler Problem 1
+
+limit = 1000
+multiples = [3, 5]
+
+answer = (1...limit).select do |n|
+  multiples.detect do |multiple|
+    n % multiple == 0
+  end
+end.inject(&:+)
+
+puts "Answer: #{answer}"
